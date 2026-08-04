e11c8d81d2a1af452168300823412d8130b468cd
vini <vinidemoraes05@gmail.com>
2026-08-03 23:47:06 -0300

    fix: fix gitflow and delete .kosho

--- .kosho/models/user.js
+++
@@ -1,3 +0,0 @@
-import mongoose from "mongoose";
-import { userSchema } from "../schemas/user.js";
-const user = mongoose.model("User", userSchema);
--- .kosho/public/index.js
+++
@@ -1,7 +0,0 @@
-function OpenComments(postElement) {
-    const comments = postElement.nextElementSibling;
-    if (comments) {
-        comments.classList.toggle("active");
-    }
-}
-export {};
